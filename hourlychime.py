41c7e234df03fb11b6a11bd853dd1981c8ae7a1e
Sukhbinder Singh <sukhbinder.singh3@rolls-royce.com>
2020-01-03 11:46:42 +0000

    working now

--- hourlychime.py
+++ hourlychime.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 import time
 import argparse
+import datetime
 
+TEXT= {0:"Hourly", 15:"15 Mins", 30:"Half-Hourly"}
 def play_beep():
     print ("\a \a")
 
@@ -12,6 +14,7 @@ def chime(mins):
         mins_in = [0,30]
     elif mins == 15:
         mins_in = [0,15,30,45]
+    print("{} Chimer on".format(TEXT[mins]))
     while True:
         try:
             now = datetime.datetime.now()
@@ -24,14 +27,14 @@ def chime(mins):
 
 def main():
     """
-    Hourly chime
+    Hourly chimer
     
     chime will do hourly chime
     chime 15 will do chime in 15 mins
     chime 30 will do chime in 30 mins
     """
     parser = argparse.ArgumentParser(description="Hourly Chimer")
-    parser.add_argument("-m","--minutes",, choices=[0,15,30] type=int, help="Time in seconds", default=0)
+    parser.add_argument("-m","--minutes", choices=[0,15,30] ,type=int, help="Time in seconds", default=0)
     args = parser.parse_args()
     chime(args.minutes)
 
